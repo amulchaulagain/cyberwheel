0458527ba65de6a588c9b36773cf3324f28eb647
Austria, Phillipe <austriaps@ornl.gov>
2024-11-26 22:14:29 +0000

    Resolve "add emulator deploy decoy host"

--- cyberwheel/tests/test_emulator_blue_actions.py
+++ cyberwheel/tests/test_emulator_blue_actions.py
@@ -1,16 +1,20 @@
 """
-Module to test the EmulatorDetector class.
+Module to test the Blue Agent actions in the emulator.
 """
 
 import unittest
 from cyberwheel.emulator.actions.blue_actions import EmulateDeployDecoyHost
 from cyberwheel.network.network_base import Network
 
+
 class TestEmulatorBlueActions(unittest.TestCase):
     """Unit tests for the the emulator blue actions"""
 
     def test_deploy_decoy_host(self) -> None:
-        """Test deploying decoy hosts in emulator"""
-        decoy = EmulateDeployDecoyHost(network=Network("test"), configs={})
-        action_result = decoy.execute()
+        """Test deploying decoy host"""
+        action = EmulateDeployDecoyHost(network=Network("test"), configs={})
+        decoy_hostname = "user02"
+        shell_cmd = action.build_emulator_cmd(decoy_hostname)
+
+        action_result = action.emulator_execute(shell_cmd)
         self.assertTrue(action_result.success)
