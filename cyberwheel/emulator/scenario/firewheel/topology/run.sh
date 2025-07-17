deac569ea564b239ff15fb509e67be99b2304697
Phillipe <austriaps@ornl.gov>
2025-07-17 20:39:35 +0000

    update run script

--- cyberwheel/emulator/scenario/firewheel/topology/run.sh
+++ cyberwheel/emulator/scenario/firewheel/topology/run.sh
@@ -3,27 +3,29 @@
 # This script starts the CyberWheel experiement in FireWheel.
 # The configuration file must be included when executing the script:
 #
-#                ./run <config_name>
+#                ./run <absolute_location/config_name.yaml>
 #
-# The available configurations can be found in the ./configs folder.
+# The available configurations can be found in the /cyberwheel/data/configs/network folder.
 
-# NETWORK_CONFIG_DIR="../../../data/configs/network"
 NETWORK_CONFIG=$1
 
 if [[ $# -eq 0 ]]; then
-    echo "Missing config name as argrument. See './configs' folder and use './run <config_name>.yaml'"
+    echo "Missing network config name argrument. Include the absolute location along with config name. 
+See 'cyberwheel/data/configs/network' folder for available configs and use './run <absolute_location/config_name>.yaml'.
+(e.g. ./run path_to_cyberwheel/cyberwheel/data/configs/network/emulator_example_config.yaml)"
     exit 1
 fi
 
 if [[ $# -gt 1 ]]; then
-    echo "Invalid number of agruments. See './configs' folder and use './run <config_name>.yaml'"
+    echo "Invalid number of agruments, there is only 1 argument for the network config file.
+Include the absolute location along with config name. See 'cyberwheel/data/configs/network' folder and use './run <absolute_location/config_name>.yaml'.
+(e.g. ./run path_to_cyberwheel/cyberwheel/data/configs/network/emulator_example_config.yaml)"
     exit 1
 fi
 
-# file=$1
-
 if [ ! -f "$NETWORK_CONFIG" ]; then
-    echo "Configuration file not found in 'configs' folder. Ensure file exist and that '.yaml' is included in the name."
+    echo "Network config file not found. Ensure file exist and that '.yaml' is included in the name.
+(e.g. ./run path_to_cyberwheel/data/configs/network/emulator_example_config.yaml)"
     exit 1
 fi
 
